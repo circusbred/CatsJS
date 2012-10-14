11f250762ed2c148bce1a6edbdaa80d0c1c689f8
Stephen Mathieson <smath23@gmail.com>
2012-10-14 15:32:36 -0400

    Quick normalization of events

--- src/event.js
+++ src/event.js
@@ -3,7 +3,8 @@
 /**
  * Events
  */
-var on, off, preventDefault, stopPropagation;
+var on, off, preventDefault, stopPropagation, normalize;
+
 if (document.addEventListener) {
 	on = function (node, type, fn) {
 		'use strict';
@@ -20,16 +21,62 @@ if (document.addEventListener) {
 
 	// IE
 } else {
+
 	preventDefault = function () {
 		'use strict';
 		this.returnValue = false;
 	};
+
 	stopPropagation = function () {
 		'use strict';
 		this.cancelBubble = true;
 	};
+
+	/**
+	 * Normalizes the event object by adding:
+	 *
+	 * * offset[X/Y]
+	 * * relatedTarget
+	 * * which
+	 * * preventDefault
+	 * * stopPropagation
+	 *
+	 * @param  {Event} e The event to be normalized
+	 * @return {Event}   The normalized event
+	 */
+	normalize = function (e) {
+		'use strict';
+
+		if (!e) {
+			e = window.event;
+		}
+
+		if (e.layerX) {
+			e.offsetX = e.layerX;
+			e.offsetY = e.layerY;
+		}
+
+		if (!e.relatedTarget) {
+			if (e.type === 'mouseover') {
+				e.relatedTarget = e.fromElement;
+			} else if (e.type === 'mouseout') {
+				e.relatedTarget = e.toElement;
+			}
+		}
+
+		e.which = e.keyCode || e.charCode;
+
+		if (typeof e.preventDefault !== 'function') {
+			e.preventDefault = preventDefault;
+			e.stopPropagation = stopPropagation;
+		}
+
+		return e;
+	};
+
 	on = function (node, type, fn) {
 		'use strict';
+
 		var f;
 		if (node.attachEvent) {
 			if (fn[expando]) {
@@ -37,24 +84,23 @@ if (document.addEventListener) {
 			} else {
 
 				f = fn[expando] = function (e) {
-					if (typeof e.preventDefault !== 'function') {
-						e.preventDefault = preventDefault;
-						e.stopPropagation = stopPropagation;
-					}
-					fn.call(node, e);
+					fn.call(node, normalize(e));
 				};
 
 			}
 			node.attachEvent('on' + type, f);
 		}
 	};
+
 	off = function (node, type, fn) {
 		'use strict';
+
 		if (node.detachEvent) {
 			node.detachEvent('on' + type, fn[expando] || fn);
 		}
 	};
 }
+
 Library.on = on;
 Library.off = off;
 
@@ -74,6 +120,7 @@ if (document.createEvent) {
 	};
 }
 Library.fire = fire;
+
 each(['on', 'off', 'fire'], function (val) {
 	'use strict';
 	proto[val] = function () {

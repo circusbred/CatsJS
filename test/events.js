d3aa713e937b7d37f1feedaadfbc2b541284f535
Stephen Mathieson <smath23@gmail.com>
2012-10-17 07:54:17 -0400

    A bit of cleanup and adding test for theLibrary.events()

--- test/events.js
+++ test/events.js
@@ -44,40 +44,50 @@
 		'</div> <!-- /container -->';
 
 
-	module("events", {
+	module('events', {
 		setup: function () {
 			document.getElementById('qunit-fixture').innerHTML = fixtureHTML;
 		}
 	});
 
-	test("on()", function () {
-		expect(5);
-		var $text = theLibrary('#text1');
+	test('on()', function () {
+		expect(7);
 
-		stop();
-		var focus = function () {
-				ok(true, 'focus fired for element: ' + this);
-				deepEqual($text[0], this, 'Context is the element');
-				start();
-			};
+		var focus, click, $a,
+			$text = theLibrary('#text1');
+
+		stop(); // why?
+
+		focus = function () {
+			ok(true, 'focus fired for element: ' + this);
+			deepEqual($text[0], this, 'Context is the element');
+			start(); // uh?
+		};
 
 		$text.on('focus', focus);
 		$text[0].focus();
 
-		document.location.hash = '';
-		var click = function (e) {
-				equal(typeof e.preventDefault, 'function', 'Prevent default is present on the event object');
-				equal(typeof e.stopPropagation, 'function', 'stopPropogation is present on the event object');
-				e.preventDefault();
-			};
+		ok($text[0].data.events.focus, 'Should have added the focus-event in the data namespace.');
 
-		var $a = theLibrary('#anchor2');
+		document.location.hash = '';
+		click = function (e) {
+			equal(typeof e.preventDefault, 'function', 'Prevent default is present on the event object');
+			equal(typeof e.stopPropagation, 'function', 'stopPropogation is present on the event object');
+			e.preventDefault();
+		};
+
+		$a = theLibrary('#anchor2');
 		$a.on('click', click);
+
+
+		ok($a[0].data.events.click, 'Should have added the click-event in the data namespace.');
+
+
 		$a.fire('click');
 		equal(document.location.hash.replace('#', ''), '', 'Default action prevented');
 	});
 
-	test("off()", function () {
+	test('off() with arguments', function () {
 		expect(3);
 		var $a = theLibrary('#link1');
 		var cnt = 0;
@@ -92,7 +102,28 @@
 		equal(cnt, 2, 'Click unbound correctly and only fired 2 times');
 	});
 
-	test("fire()", function () {
+	test('off() without fn', function () {
+
+		expect(3);
+
+		var $a = theLibrary('#link1'),
+			cnt = 0,
+			click = function (e) {
+				deepEqual($a[0], this, 'Click context is the element');
+				cnt += 1;
+			};
+
+		$a.on('click', click);
+		$a.fire('click').fire('click');
+
+		$a.off('click');
+		$a.fire('click').fire('click');
+
+		equal(cnt, 2, 'Click unbound correctly and only fired 2 times.');
+
+	});
+
+	test('fire()', function () {
 		var $a = theLibrary('#link1');
 		var cnt = 0;
 		var click = function () {
@@ -103,4 +134,35 @@
 		$a.fire('click').fire('click').fire('click');
 		equal(cnt, 3, 'Click fired three times');
 	});
+
+	test('events()', function () {
+
+		expect(6);
+
+		var events,
+			$a = theLibrary('#link1'),
+			click = function (e) {
+				return true;
+			},
+			focus = function (e) {
+				return true;
+			},
+			blur = function (e) {
+				return true;
+			};
+
+		$a.on('click', click).on('focus', focus).on('blur', blur);
+
+		events = $a.events();
+
+		ok(events.click, '');
+		ok(events.focus, '');
+		ok(events.blur, '');
+
+		deepEqual(events.click[0], click, '');
+		deepEqual(events.focus[0], focus, '');
+		deepEqual(events.blur[0], blur, '');
+
+	});
+
 }());

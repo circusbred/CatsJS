d0309d3b2ffdf578aa7f13f5313fab3d9b256afd
Stephen Mathieson <smath23@gmail.com>
2012-11-17 10:05:35 -0500

    improved coverage

--- test/dom.js
+++ test/dom.js
@@ -1,3 +1,4 @@
+/*jslint browser:true*/
 /*global module, test, theLibrary, equal*/
 (function () {
 	'use strict';
@@ -20,6 +21,12 @@
 
 		equal($fixture.html(), expected, 'should get innerHTML correctly');
 
+		fixture.innerHTML = '';
+		equal($fixture.html(), '', 'should return an empty string when there is not innerHTML');
+
+		$fixture = theLibrary('#cats-and-dogs-and-smurfs-and-things-not-real');
+		equal($fixture.html(), '', 'should return an empty string on missing elements');
+
 	});
 
 
@@ -39,7 +46,7 @@
 
 		$paragraphs = $fixture.find('p');
 
-		$paragraphs.remove();
+		equal($paragraphs.remove(), undefined, 'should not be chainable');
 
 		equal($fixture.find('p').length, 0, 'should remove each member of a collection');
 	});
@@ -48,6 +55,7 @@
 	test('library#append', function () {
 
 		var index,
+			div = document.createElement('div'),
 			$fixture = theLibrary(fixture),
 			paragraphs = '<p>cats1</p>' +
 							'<p>cats2</p>' +
@@ -57,10 +65,22 @@
 							'<p>cats6</p>' +
 							'<p>cats7</p>';
 
+		div.className = 'cats';
+		div.innerHTML = 'dogs';
+
+		$fixture.append('<div>cats</div>');
+		equal($fixture.find('div')[0].innerHTML, 'cats', 'should insert strings correctly');
+		$fixture.html('');
+
+		$fixture.append(div);
+
+		equal($fixture.find('div')[0], div, 'should add nodes correctly');
+
 		$fixture.append(paragraphs);
 
 		equal($fixture.find('p').length, 7, 'should append strings correctly');
-		$fixture.html('');
+
+		$fixture.html('').append(div);
 
 		paragraphs = [];
 		for (index = 0; index < 7; index += 1) {
@@ -69,13 +89,79 @@
 		}
 
 		$fixture.append(paragraphs);
+
+		// should have 7 paragraphs
 		equal($fixture.find('p').length, 7, 'should append lists correctly');
 
+		// should insert in correct order
+		for (index = 0; index < 8; index += 1) {
+
+			if (index === 0) {
+
+				equal($fixture[0].childNodes[index], div, 'div should be the first');
+
+			} else {
+
+				equal($fixture[0].childNodes[index], paragraphs[index - 1], 'should maintain list order');
+
+			}
+
+		}
+
 	});
 
-/*
 	test('library#prepend', function () {
+		var index,
+			div = document.createElement('div'),
+			$fixture = theLibrary(fixture),
+			paragraphs = '<p>cats1</p>' +
+							'<p>cats2</p>' +
+							'<p>cats3</p>' +
+							'<p>cats4</p>' +
+							'<p>cats5</p>' +
+							'<p>cats6</p>' +
+							'<p>cats7</p>';
+
+		div.className = 'cats';
+		div.innerHTML = 'dogs';
+
+		$fixture.prepend('<div>cats</div>');
+		equal($fixture.find('div')[0].innerHTML, 'cats', 'should insert strings correctly');
+		$fixture.html('');
+
+		$fixture.prepend(div);
+
+		equal($fixture.find('div')[0], div, 'should add nodes correctly');
+
+		$fixture.prepend(paragraphs);
+
+		equal($fixture.find('p').length, 7, 'should append strings correctly');
+		$fixture.html('');
+
+		paragraphs = [];
+		// make a bunch of paragraphs
+		for (index = 0; index < 7; index += 1) {
+			paragraphs[index] = document.createElement('p');
+			paragraphs[index].innerHTML = 'cats' + index.toString();
+		}
+
+		// add a child to the fixture
+		$fixture.append(div);
+
+		// add the paragraphs
+		$fixture.prepend(paragraphs);
+
+		// should have 7 paragraphs
+		equal($fixture.find('p').length, 7, 'should append lists correctly');
+
+		// last child should be the div
+		equal($fixture[0].lastChild, div, 'div should be the last child');
+
+		// should insert in correct order
+		for (index = 0; index < 7; index += 1) {
+			equal($fixture[0].childNodes[index], paragraphs[index], 'should maintain list order');
+		}
 
 	});
-*/
+
 }());
